f58f0499a34eb8bb4d4b5e0883a29bf7ec25d2ab
GastonGit <73943682+GastonGit@users.noreply.github.com>
2022-01-23 16:23:04 +0100

    init grid image list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,157 @@
 import React from 'react';
 import '../styles/App.css';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 
-class App extends React.Component<unknown, unknown> {
-    constructor(props: unknown) {
-        super(props);
-    }
-
-    render(): JSX.Element {
-        return (
-            <div className="app">
-                <header className="app__header">
-                    <p></p>
-                </header>
-            </div>
-        );
-    }
+export default function App(): JSX.Element {
+    return (
+        <ImageList sx={{ width: 1800 }} cols={7} gap={8} rowHeight={380}>
+            {itemData.map((item) => (
+                <ImageListItem key={item.img}>
+                    <img
+                        src={`${item.img}`}
+                        srcSet={`${item.img}`}
+                        alt={item.title}
+                    />
+                    <ImageListItemBar
+                        title={item.title}
+                        subtitle={<span>by: {item.author}</span>}
+                        position="below"
+                    />
+                </ImageListItem>
+            ))}
+        </ImageList>
+    );
 }
 
-export default App;
+// export default function App(): JSX.Element {
+//     return (
+//         <ImageList cols={7} gap={8} rowHeight={380}>
+//             {itemData.map((item) => (
+//                 <ImageListItem key={item.img}>
+//                     <a className="image__url" href="#">
+//                         <img
+//                             src={`${item.img}`}
+//                             srcSet={`${item.img}`}
+//                             alt={item.title}
+//                         />
+//                     </a>
+//                     <ImageListItemBar
+//                         title={item.title}
+//                         subtitle={<span>by: {item.author}</span>}
+//                         position="below"
+//                     />
+//                 </ImageListItem>
+//             ))}
+//         </ImageList>
+//     );
+// }
+
+const itemData = [
+    {
+        img: 'logo512.png',
+        title: 'Breakfast',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Burger',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Camera',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Coffee',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Hats',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Honey',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Basketball',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Fern',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Mushrooms',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Tomato basil',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Sea star',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Bike',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Coffee',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Hats',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Honey',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Basketball',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Fern',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Mushrooms',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Tomato basil',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Sea star',
+        author: '@test',
+    },
+    {
+        img: 'logo512.png',
+        title: 'Bike',
+        author: '@test',
+    },
+];
