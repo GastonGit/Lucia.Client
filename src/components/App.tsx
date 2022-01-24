89efbcd204ecbd0b1b1633f9e6b277b038eae818
GastonGit <73943682+GastonGit@users.noreply.github.com>
2022-01-24 15:56:47 +0100

    setup inital fetching from server

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/App.css';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
@@ -6,132 +6,56 @@ import ImageListItemBar from '@mui/material/ImageListItemBar';
 import { Link } from '@mui/material';
 
 export default function App(): JSX.Element {
-    return (
-        <ImageList sx={{ width: 1800 }} cols={7} gap={8} rowHeight={380}>
-            {itemData.map((item, index) => (
-                <Link key={item.img + (index + 1)} href="#">
-                    <ImageListItem>
-                        <img
-                            src={`${item.img}`}
-                            srcSet={`${item.img}`}
-                            alt={item.title}
-                        />
-                        <ImageListItemBar
-                            title={item.title}
-                            subtitle={<span>by: {item.author}</span>}
-                            position="below"
-                        />
-                    </ImageListItem>
-                </Link>
-            ))}
-        </ImageList>
-    );
-}
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
 
-const itemData = [
-    {
-        img: 'logo512.png',
-        title: 'Breakfast',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Burger',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Camera',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Coffee',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Hats',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Honey',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Basketball',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Fern',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Mushrooms',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Tomato basil',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Sea star',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Bike',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Coffee',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Hats',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Honey',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Basketball',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Fern',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Mushrooms',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Tomato basil',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Sea star',
-        author: '@test',
-    },
-    {
-        img: 'logo512.png',
-        title: 'Bike',
-        author: '@test',
-    },
-];
+    useEffect(() => {
+        fetch(process.env.REACT_APP_API_SERVER || '')
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                },
+            );
+    }, []);
+
+    if (error) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <span>Loading...</span>;
+    } else {
+        return (
+            <ImageList sx={{ width: 1800 }} cols={7} gap={8} rowHeight={380}>
+                {items.map(
+                    (
+                        item: { img: string; title: string; author: string },
+                        index: number,
+                    ) => (
+                        <Link key={item.img + (index + 1)} href="#">
+                            <ImageListItem>
+                                <img
+                                    src={`${item.img}`}
+                                    srcSet={`${item.img}`}
+                                    alt={item.title}
+                                />
+                                <ImageListItemBar
+                                    title={item.title}
+                                    subtitle={<span>by: {item.author}</span>}
+                                    position="below"
+                                />
+                            </ImageListItem>
+                        </Link>
+                    ),
+                )}
+            </ImageList>
+        );
+    }
+}
