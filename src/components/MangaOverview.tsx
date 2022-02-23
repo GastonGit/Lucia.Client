00be7a5c02b0c952f3df4751e88372e6f90ae322
GastonGit <73943682+GastonGit@users.noreply.github.com>
2022-02-23 16:24:08 +0100

    fix MangaOverview misusing server api

--- src/components/MangaOverview.tsx
+++ src/components/MangaOverview.tsx
@@ -1,24 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/App.css';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
 import { useLocation } from 'react-router-dom';
-import { Link } from '@mui/material';
+import ImageList from '@mui/material/ImageList';
+import { Link, Stack, Typography } from '@mui/material';
+import ImageListItem from '@mui/material/ImageListItem';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function MangaOverview(): JSX.Element {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState({
+    const [manga, setManga] = useState({
+        information: {
+            author: '',
+            bucket: '',
+            chapter: 1,
+            created_at: '',
+            directory: '',
+            id: 0,
+            series: '',
+            thumbnail: '',
+            title: '',
+            universe: '',
+        },
         images: [],
-        title: '',
-        author: '',
-        id: -1,
     });
 
     const api = process.env.REACT_APP_API_SERVER || '';
     const location = useLocation();
-    console.log(location);
 
     useEffect(() => {
         fetch(api + location.pathname)
@@ -26,7 +33,7 @@ export default function MangaOverview(): JSX.Element {
             .then(
                 (result) => {
                     setIsLoaded(true);
-                    setItems(result);
+                    setManga(result);
                 },
                 (error) => {
                     setIsLoaded(true);
@@ -43,40 +50,43 @@ export default function MangaOverview(): JSX.Element {
         return <span>Loading...</span>;
     } else {
         return (
-            <div>
-                <p>Title: {items.title}</p>
+            <Stack
+                direction="column"
+                alignItems="center"
+                spacing={2}
+                mt={2}
+                mb={2}
+            >
+                <Typography variant="h2" color="var(--main-text-color)">
+                    {manga.information.title}
+                </Typography>
                 <ImageList
-                    sx={{ width: 1800 }}
+                    sx={{
+                        width: 1700,
+                        borderStyle: 'solid',
+                        borderWidth: '5px',
+                        borderColor: 'var(--quinary--bg-color)',
+                        backgroundColor: 'var(--quinary--bg-color)',
+                    }}
                     cols={7}
-                    gap={8}
                     rowHeight={380}
+                    gap={6}
                 >
-                    {items.images.map(
-                        (
-                            item: {
-                                img: string;
-                                title: string;
-                                author: string;
-                                id: string;
-                            },
-                            index: number,
-                        ) => (
-                            <Link
-                                key={item + '' + (index + 1)}
-                                href={
-                                    '/read/' + items.id + '?page=' + (index + 1)
-                                }
-                            >
-                                <ImageListItem>
-                                    <img
-                                        src={`http://localhost:6021/${item}`}
-                                    />
-                                </ImageListItem>
-                            </Link>
-                        ),
-                    )}
+                    {manga.images.map((image: { name: string }) => (
+                        <Link
+                            key={image.name}
+                            href={'/manga/' + manga.information.id}
+                        >
+                            <ImageListItem>
+                                <img
+                                    src={`${process.env.REACT_APP_API_SERVER}/media/${manga.information.directory}/${manga.information.bucket}/${image.name}`}
+                                    alt={'Image for ' + manga.information.title}
+                                />
+                            </ImageListItem>
+                        </Link>
+                    ))}
                 </ImageList>
-            </div>
+            </Stack>
         );
     }
 }
