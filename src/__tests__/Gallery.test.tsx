185c5208556b39fb6794913212a40a0a0540c539
GastonGit <73943682+GastonGit@users.noreply.github.com>
2022-03-31 14:04:48 +0200

    delete unused test case

--- src/__tests__/Gallery.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../components/Gallery';
-
-test('renders boiled rice text', () => {
-    render(<App />);
-    const boilElement = screen.getByText(/Boiled rice/i);
-    expect(boilElement).toBeInTheDocument();
-});
